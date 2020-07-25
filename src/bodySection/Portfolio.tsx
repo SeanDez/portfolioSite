a33e6c55c5ec939482fa904f8e646005cae092d3
Sean Dez <sd@seandezoysa.com>
2020-07-26 02:07:14 +0700

    setup portfolio preview page

--- src/bodySection/Portfolio.tsx
+++ src/bodySection/Portfolio.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import ItemPreviewsInterFace from "./portfolioItems/ItemPreviewsInterface";
+import ThisSite from "./portfolioItems/thisSite";
 
 interface PropsShape {}
+
+const itemPreviewData: ItemPreviewsInterFace[] = [
+  { name: 'This Website', slug: 'this-site', element: <ThisSite /> }
+]
+
+function buildAllItemPreviews(previewData: ItemPreviewsInterFace[]) {
+  return previewData.map(singlePreview => (
+    <div>
+      {singlePreview.element}
+    </div>
+  ));
+}
 
 export default (props: PropsShape) => {
 
   return (
     <main>
       <p>Portfolio Here</p>
+      {() => buildAllItemPreviews(itemPreviewData)}
     </main>
   )
 }
+
---
+++ src/bodySection/portfolioItems/ItemPreviewsInterface.tsx
@@ -0,0 +1,5 @@
+export default interface ItemPreviewsInterface {
+  name: string,
+  slug: string,
+  element: JSX.Element
+}
