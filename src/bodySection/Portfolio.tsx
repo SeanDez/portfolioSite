86e6ee032b218e5c6aead2813a0da9f8de478047
Sean Dez <sd@seandezoysa.com>
2020-07-27 03:51:43 +0700

    wip setting up portfolio items

--- src/bodySection/Portfolio.tsx
+++ src/bodySection/Portfolio.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
+import { BrowserRouter, Switch, Link, Route, useRouteMatch } from 'react-router-dom';
 import ItemPreviewsInterFace from "./portfolioItems/ItemPreviewsInterface";
+
 import ThisSite from "./portfolioItems/thisSite";
 
 interface PropsShape {}
 
 const itemPreviewData: ItemPreviewsInterFace[] = [
-  { name: 'This Website', slug: 'this-site', element: <ThisSite /> }
+  { name: 'This Website', slug: '/this-site', element: <ThisSite /> }
 ]
 
 function buildAllItemPreviews(previewData: ItemPreviewsInterFace[]) {
   return previewData.map(singlePreview => (
     <div>
-      {singlePreview.element}
+      <Link to={singlePreview.slug}>{singlePreview.name}</Link>
     </div>
   ));
+}
+
+function buildAllRoutes(previewData: ItemPreviewsInterFace[]) {
+  return previewData.map(singlePreview => (
+  <Route path={`${useRouteMatch().url}${singlePreview.slug}`}>
+    {singlePreview.element}
+  </Route>
+  ))
 }
 
 export default (props: PropsShape) => {
 
   return (
-    <div>
-      <p>Portfolio Here</p>
-      {() => buildAllItemPreviews(itemPreviewData)}
-    </div>
+    <BrowserRouter>
+      <section>
+        {buildAllItemPreviews(itemPreviewData)}
+
+        {/* Routes here */}
+        <Switch>
+          {buildAllRoutes(itemPreviewData)}
+        </Switch>
+      </section>
+    </BrowserRouter>
   )
 }
 
