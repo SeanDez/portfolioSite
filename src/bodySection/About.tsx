f8d3a21eeab699c9ac936267c75d13c5ec230859
Sean Dez <sd@seandezoysa.com>
2020-08-25 18:11:32 +0700

    tweak about page text

--- src/bodySection/About.tsx
+++ src/bodySection/About.tsx
@@ -8,9 +8,11 @@ export default (props: PropsShape) => (
 
     <p>I'm a React Typescript developer currently in Miami, moving to Atlanta for greater job opportunities.</p>
 
-    <p>On the back end I use Express.js and PostgreSQL. That is my main stack. I also know PHP and Wordpress, and have written a couple small plugins (not featured because I didn't complete the last 20% or so for them to be presentable).</p>
+    <p>On the back end I use Express.js and PostgreSQL. That is my main stack.</p>
+    
+    <p>I also know PHP and Wordpress, and have written a couple small plugins. I didn't complete the last 20% or so for them to be presentable, so I didn't add them to my portfolio.</p>
 
-    <p>I'm studying AWS on the side and will probably sit for the CDA (Certified Developer Associate) exam in six to twelve months.</p>
+    <p>I'm studying AWS on the side. I'll try to sit for the Certified Developer Associate exam in six to twelve months. My long term goal is to add AWS devops pipelines to my skillset.</p>
 
     <p>I like what I do; my portfolio items were built for me. Coding is like grown-up legos without foot pain when you mis-step.</p>
   </section>
