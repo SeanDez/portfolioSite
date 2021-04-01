4291cdf0143dc8c68ecf5e8c3a72fffa16cae275
Sean Dezoysa <mrseandezoysa@gmail.com>
2021-03-31 21:17:10 -0400

    update text

--- src/bodySection/navigation/LinkList.tsx
+++ src/bodySection/navigation/LinkList.tsx
@@ -13,11 +13,11 @@ export default (props: { jsxData: Record<string, ComponentData> }) => {
       { Object.values(props.jsxData).map((nameAndSlug: ComponentData, index: number) => (
         <SingleLinkListItem linkData={nameAndSlug} key={index} />
       )) }
-      <a href="https://docs.google.com/document/d/1B04GszEveekuiB2JWZwvgZTWFpWx2J5xlsTFNHbkuFQ/edit?usp=sharing" target="_blach">
+      <a href="https://docs.google.com/document/d/1uqlRYk_m-bvDYyucfKAJ2kxvzGub-E0Q9i8BTwqxQ_Y/edit?usp=sharing" target="_blach">
         <StyledButton
           isActive={false}
           initial={{ scale: 1 }}
-         whileHover={{ 
+         whileHover={{
            textShadow: '0px 0px 2px rgb(255, 255, 255)',
            scale: 1.1
           }}
@@ -28,7 +28,7 @@ export default (props: { jsxData: Record<string, ComponentData> }) => {
       </a>
     </NavList>
   )
-} 
+}
 
 const NavList = styled(motion.ul)`
   list-style-type: none;
