9f04c0657c4e538acc950c07014f5ade331e450f
Sean Dezoysa <sean.dezoysa@equipmentshare.com>
2022-02-08 06:00:33 -0500

    small updates

--- src/bodySection/About.tsx
+++ src/bodySection/About.tsx
@@ -13,11 +13,20 @@ export default (props: PropsShape) => (
     animate='opaque'
   >
     <h2>About</h2>
-
-    <p>Working as a writer was creatively gratifying, but the puzzle-solving side of my left brain often felt neglected. I've always enjoyed the aesthetics and logical component of web development, and decided to study it part time about five years ago, soon landing my first job to work on a React.js application fed by a Node/Express backend.</p>
-
-    <p>Starting with front end feature additions and debug stories, my duties quickly expanded into the back end. I got to spend more time working on an Express.js API. I worked on my database querying skills. On my off hours I studied Typescript concepts I didn't know. Today I can confidently say I'm proficient on both sides of the stack, from React.js to Node and Express, ending at the PostgreSQL database.</p>
-    
-    <p>Coding is both my work and my passion. Contributing to something useful and important makes it easy to work long hours while enjoying the ride.</p>
+    <p>
+      Fresh out of college, a friend and I started a online lead generation business. Google and Facebook traffic with local city keywords were our focus. We would either rank highly in the "organic search", or pay for ads, capture leads, and resell them to local businesses.
+    </p>
+    <p>
+      I wore a lot of hats back then. I handled analytics, analyzing traffic flow from ad to landing page. I learned a lot about design, conversion rate optimization, WordPress,
+    </p>
+    <p>    
+      In the early days of the search engines and social media it was easy to make money. That eventually came to an end.... competition and ad prices increased. We shut the doors on the business when it no longer made sense to us. 
+    </p>
+    <p>    
+      I took my self developed skills into the world of professional IT. Modern toolsets used a JavaScript tech stack so that was a big shift. React.js, Node.js, Express.js, and MongoDB (which I swapped for PostgreSQL due to what we used at my workplace). 
+    </p>
+    <p>    
+      Professional development is a lot less hacky and a lot more accountable. I both love and hate it at times (I'm looking at you, multi day debugs!). Even with 10+ years in IT, I continue to learn new things every day from both junior and senior colleagues. That unending learning environment is the part of development I enjoy most.
+    </p>
   </motion.section>
 );
